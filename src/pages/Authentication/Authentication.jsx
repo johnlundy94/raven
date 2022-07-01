c5dbf741f6f9df53963f3b4a680f0b03c7dd001d
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-07-01 17:20:48 -0600

    Able to add user to firestore database

--- src/pages/Authentication/Authentication.jsx
+++ src/pages/Authentication/Authentication.jsx
@@ -6,7 +6,7 @@ import {
 function Authentication() {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
