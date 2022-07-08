27e23ca25ae776b03bd9b537f1acffeaf4323d4c
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2022-07-08 09:25:44 -0600

    Able to sign in with google to firestore

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -7,6 +7,7 @@ function SignIn() {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
